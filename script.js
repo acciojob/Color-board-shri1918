1c4048ae2341b7b919197df4b1faed7e49cfafeb
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-04-25 11:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get all the square elements
+const squares = document.querySelectorAll('.square');
+
+// Add event listeners to each square element
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = '#f00';
+  });
+  square.addEventListener('mouseout', () => {
+    setTimeout(() => {
+      square.style.backgroundColor = '#fff';
+    }, 1000);
+  });
+});
